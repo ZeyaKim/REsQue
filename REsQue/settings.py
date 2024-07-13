0df6224b852ef19c1a9b775e3269f0770d430ef2
ZeyaKim <chlwltjrdev@gmail.com>
2024-07-14 03:53:34 +0900

    ✨ Create requirement_testing app
    - requirement_testing 앱을 생성한 후 USER_APPS 에 등록

--- REsQue/settings.py
+++ REsQue/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-USER_APPS = ["account", "project", "requirement"]
+USER_APPS = ["account", "project", "requirement", "requirement_testing"]
 # Application definition
 
 INSTALLED_APPS = [
---
+++ requirement_testing/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RequirementTestingConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "requirement_testing"
