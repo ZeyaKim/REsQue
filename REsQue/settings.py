f733fb39d4ce35729d52b89d647f1b9b78aceb93
ZeyaKim <chlwltjrdev@gmail.com>
2024-07-14 03:54:23 +0900

    ✨ Create builder app
    - builder 앱을 생성하고 USER_APPS에 추가하였습니다.

--- REsQue/settings.py
+++ REsQue/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-USER_APPS = ["account", "project", "requirement", "requirement_testing"]
+USER_APPS = ["account", "project", "requirement", "requirement_testing", "builder"]
 # Application definition
 
 INSTALLED_APPS = [
