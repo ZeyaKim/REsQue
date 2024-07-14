a758c3e09fb8eaa6d54b67e32e581e5046647769
ZeyaKim <chlwltjrdev@gmail.com>
2024-07-14 19:38:24 +0900

    🌟 feat: Implement FakeModel class for test data generation
    - FakeModel 클래스 구현으로 테스트 데이터 생성 기능 추가
    - 테스트의 신뢰성과 유지보수성 향상을 위한 가짜 모델 생성 시스템 도입

---
+++ dev/test/fake_data/models/fake_account_models.py
@@ -0,0 +1,12 @@
+from faker import Faker
+from dev.test.fake_data.models.fake_model import FakeModel
+from account.models import CustomUser
+
+
+class FakeCustomUser(FakeModel):
+    def __init__(self):
+        super().__init__(CustomUser)
+
+    @property
+    def required_fields(self):
+        return {"email": self._faker.email(), "password": self._faker.password()}
---
+++ dev/test/fake_data/models/fake_model.py
@@ -0,0 +1,33 @@
+from faker import Faker
+import abc
+from django.db.models import Model
+from typing import Type, Any
+
+fake = Faker()
+
+
+class FakeModel(abc.ABC):
+    def __init__(self, model: Type[Model]):
+        self._faker = fake
+        self._model = model
+        self._instance: Model | None = None
+        self._required_fields: dict[str, Any] | None = None
+
+    @abc.abstractmethod
+    def set_required_fields(self) -> dict[str, Any]:
+        """
+        하위 클래스에서 반드시 구현해야 하는 추상 메서드
+        """
+        pass
+
+    @property
+    def required_fields(self) -> dict[str, Any]:
+        if self._required_fields is None:
+            self._required_fields = self.set_required_fields()
+        return self._required_fields
+
+    @property
+    def instance(self) -> Model:
+        if self._instance is None:
+            self._instance = self._model(**self.required_fields)
+        return self._instance
