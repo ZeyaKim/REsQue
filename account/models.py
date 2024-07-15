f96379ad31cf22513cda40070e792e1e85378eee
ZeyaKim <chlwltjrdev@gmail.com>
2024-07-16 00:58:30 +0900

    ✨ Add empty implementation for user login
    - 빈 urls.py와 models.py 생성

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager): ...
+
+
+class CustomUser(AbstractBaseUser):
+    def __str__(self):
+        return self.uuid
---
+++ account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "account"
+
+urlpatterns = [
+    # 여기에 URL 패턴을 추가하세요
+    # 예: path('login/', views.login_view, name='login'),
+]
