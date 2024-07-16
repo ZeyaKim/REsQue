14fd06f17066315690554935b7f053ad4cd8aec8
ZeyaKim <chlwltjrdev@gmail.com>
2024-07-16 22:48:49 +0900

    ✅ : Add & Pass test test_pass_validation_email_within_max_length
    - 올바른 길이의 이메일이 전달되었을 때 ValidationError가 반환되지 않는것을 테스트

--- account/tests/unit_test/test_validators.py
+++ account/tests/unit_test/test_validators.py
@@ -3,18 +3,34 @@ from django.core.exceptions import ValidationError
 from account.validators import validate_email_length
 
 
-class RegisterValidatorTestCase(SimpleTestCase):
+class ValidatorTestCase(SimpleTestCase):
     def setUp(self): ...
 
-    def test_validate_email_length(self):
+    def test_raise_validation_error_when_email_is_too_long(self):
+        # Arrange
         data = {
             "email": "dsfsdfsdfdsfsdfssfsdfdsfsdfsfsfsdfdsfsdfsfsfsdfdsfsdfsfsfsdfdsfsdfsf@naver.com",
             "password": "jason1234",
         }
 
+        # Act
         with self.assertRaises(ValidationError) as e:
             validate_email_length(**data)
 
+        # Assert
         self.assertEqual(
             str(e.exception.message), "Ensure this value has at most 50 characters."
         )
+
+    def test_pass_validation_email_within_max_length(self):
+        # Arrange
+        data = {
+            "email": "dsfsdfsdfdsfsdfssff@naver.com",
+            "password": "jason1234",
+        }
+
+        # Act
+        try:
+            validate_email_length(**data)
+        except ValidationError:
+            self.fail()
