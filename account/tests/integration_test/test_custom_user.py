26c8c0b7751b8853423229d95030ba2527c37046
ZeyaKim <chlwltjrdev@gmail.com>
2024-07-19 16:56:34 +0900

    🧪 Add failing tests
    - 실패하는 테스트들 추가, 요구사항 정리 이후 추가 수정

--- account/tests/integration_test/test_custom_user.py
+++ account/tests/integration_test/test_custom_user.py
@@ -10,16 +10,57 @@ class CustomUserSignUpTestCase(APITestCase):
         self.user_model = get_user_model()
         self.sign_up_url = reverse("sign-up")
 
-    def test_sign_up_success(self):
+    def test_pass_sign_up(self):
+        # Arrange
         initial_user_count = get_user_model().objects.count()
-
         self.new_user = FakeCustomUser()
 
         data = self.new_user.required_fields
 
+        # Act
         response = self.client.post(self.sign_up_url, data)
 
+        # Assert
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data["email"], data["email"])
-
         self.assertEqual(get_user_model().objects.count(), initial_user_count + 1)
+
+    def test_fail_sign_up_with_too_long_email(self):
+        # Arrange
+        ...
+
+    def test_fail_sign_up_with_too_short_email(self):
+        # Arrange
+        ...
+
+    def test_fail_sign_up_with_duplicated_email(self):
+        # Arrange
+        ...
+
+    def test_fail_sign_up_with_invalid_email(self):
+        # Arrange
+        ...
+
+    def test_fail_sign_up_with_data_without_email(self):
+        # Arrange
+        ...
+
+    def test_fail_sign_up_with_too_short_password(self):
+        # Arrange
+        ...
+
+    def test_fail_sign_up_with_too_long_password(self):
+        # Arrange
+        ...
+
+    def test_fail_sign_up_with_password_without_number(self):
+        # Arrange
+        ...
+
+    def test_fail_sign_up_with_password_without_alphabet(self):
+        # Arrange
+        ...
+
+    def test_fail_sign_up_with_data_without_password(self):
+        # Arrange
+        ...
