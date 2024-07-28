247879f51868f1249f8e39ffa5a55c3b30f9fab4
ZeyaKim <chlwltjrdev@gmail.com>
2024-07-29 00:50:49 +0900

    🧪 : Add integration tests for user sign-in functionality

---
+++ account/tests/integration_test/test_user_signin.py
@@ -0,0 +1,46 @@
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+User = get_user_model()
+
+
+class UserSignInTestCase(APITestCase):
+    def setUp(self):
+        self.user_info = {"email": "testuser@email.com", "password": "TestPassword123!"}
+
+        self.user = User.objects.create_user(**self.user_info)
+        self.url = reverse("sign-in")
+
+    def test_success_login_with_valid_credentials(self):
+        """
+        Test user can login with valid credentials
+        """
+
+        # When
+        response = self.client.post(self.url, self.user_info)
+
+        # Then
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("access_token", response.data)
+        self.assertIn("refresh_token", response.data)
+
+    def test_fail_login_with_invalid_password(self):
+        # Given
+        self.user_info["password"] = "InvalidPassword123!"
+
+        # When
+        response = self.client.post(self.url, self.user_info)
+
+        # Then
+        self.assertEqual(response.status_code, 400)
+
+    def test_fail_login_with_nonexistent_account(self):
+        # Given
+        self.user_info["email"] = "nonexistuser@email.com"
+
+        # When
+        response = self.client.post(self.url, self.user_info)
+
+        # Then
+        self.assertEqual(response.status_code, 400)
