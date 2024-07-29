357a9ab62bfa12647a0055adb722530206993fc9
ZeyaKim <chlwltjrdev@gmail.com>
2024-07-29 23:43:50 +0900

    refactor: Improve user sign-in logic and error handling

--- account/views.py
+++ account/views.py
@@ -16,24 +16,36 @@ class SignUpView(generics.CreateAPIView):
 
 class SignInView(APIView):
     def post(self, request):
-        serializer = UserSignInSerializer(data=request.data)
-        if serializer.is_valid():
-            email = serializer.validated_data["email"]
-            password = serializer.validated_data["password"]
-            user = authenticate(request, email=email, password=password)
 
-            if user is not None:
-                refresh = RefreshToken.for_user(user)
-                return Response(
-                    {
-                        "refresh": str(refresh),
-                        "access": str(refresh.access_token),
-                    },
-                    status=status.HTTP_200_OK,
-                )
-            else:
-                return Response(
-                    {"error": "Invalid credentials"},
-                    status=status.HTTP_401_UNAUTHORIZED,
-                )
+        serializer = UserSignInSerializer(data=request.data)
+        print(f"request.data: {request.data}\nis_valid: {serializer.is_valid()}")
+
+        if serializer.is_valid():
+            email = serializer.validated_data["email"].strip()
+            password = serializer.validated_data["password"]
+
+            try:
+                user = User.objects.get(email=email)
+
+                if not user.is_active:
+                    return Response(
+                        {"error": "User account is disabled"},
+                        status=status.HTTP_401_UNAUTHORIZED,
+                    )
+                if user.check_password(password):
+                    refresh = RefreshToken.for_user(user)
+                    return Response(
+                        {
+                            "refresh": str(refresh),
+                            "access": str(refresh.access_token),
+                        },
+                        status=status.HTTP_200_OK,
+                    )
+            except User.DoesNotExist:
+                pass
+
+            return Response(
+                {"error": "Invalid credentials"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
