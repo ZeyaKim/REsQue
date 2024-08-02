929f7a27a241864fe7fb60bdab298dfbaea77bb4
ZeyaKim <chlwltjrdev@gmail.com>
2024-08-02 19:18:48 +0900

    ✨ : Add Project model with title, description, timestamps, founder, and is_active field

--- project/models.py
+++ project/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    founder = models.ForeignKey(
+        "account.CustomUser", on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self):
+        return self.title
