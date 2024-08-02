47d139576b9c7172ee3c0423767a5e8193e12ade
ZeyaKim <chlwltjrdev@gmail.com>
2024-08-02 22:44:08 +0900

    🧪 : Add validation tests for Project creation

--- project/tests/unit_test/test_project_creation_validation.py
+++ project/tests/unit_test/test_project_creation_validation.py
@@ -1,22 +1,123 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
+from django.test import SimpleTestCase
+from project.serializers import ProjectSerializer
 
 
-class ProjectCreationValidationTestCase(TestCase):
-    def setUp(self): ...
+class ProjectCreationValidationTestCase(SimpleTestCase):
+    def setUp(self):
+        pass
 
-    def test_project_creation_with_valid_data(self): ...
+    def create_valid_project_data(self):
+        return {
+            "title": "Test Project",
+            "description": "This is a test project.",
+        }
 
-    def test_project_creation_with_empty_title(self): ...
+    def test_project_creation_with_valid_data(self):
+        # Given
+        valid_project_data = self.create_valid_project_data()
 
-    def test_project_creation_with_too_long_title(self): ...
+        # When
+        project_serializer = ProjectSerializer(data=valid_project_data)
 
-    def test_project_creation_with_too_long_description(self): ...
+        # Then
+        self.assertTrue(project_serializer.is_valid())
 
-    def test_project_creation_with_missing_title(self): ...
+    def test_project_creation_with_empty_title(self):
+        # Given
+        invalid_project_data = self.create_valid_project_data()
+        invalid_project_data["title"] = ""
 
-    def test_project_creation_with_missing_description(self): ...
+        # When
+        project_serializer = ProjectSerializer(data=invalid_project_data)
 
-    def test_project_creation_with_non_string_title(self): ...
+        # Then
+        self.assertFalse(project_serializer.is_valid())
+        self.assertEqual(
+            project_serializer.errors["title"][0], "This field may not be blank."
+        )
 
-    def test_project_creation_with_non_string_description(self): ...
+    def test_project_creation_with_too_long_title(self):
+        # Given
+        invalid_project_data = self.create_valid_project_data()
+        invalid_project_data["title"] = "a" * 101
+
+        # When
+        project_serializer = ProjectSerializer(data=invalid_project_data)
+
+        # Then
+        self.assertFalse(project_serializer.is_valid())
+        self.assertEqual(
+            project_serializer.errors["title"][0],
+            "이 필드의 글자 수가 100 이하인지 확인하십시오.",
+        )
+
+    def test_project_creation_with_too_long_description(self):
+        # Given
+        invalid_project_data = self.create_valid_project_data()
+        invalid_project_data["description"] = "a" * 1001
+
+        # When
+        project_serializer = ProjectSerializer(data=invalid_project_data)
+
+        # Then
+        self.assertFalse(project_serializer.is_valid())
+        self.assertEqual(
+            project_serializer.errors["description"][0],
+            "Ensure this field has no more than 1000 characters.",
+        )
+
+    def test_project_creation_with_missing_title(self):
+        # Given
+        invalid_project_data = self.create_valid_project_data()
+        del invalid_project_data["title"]
+
+        # When
+        project_serializer = ProjectSerializer(data=invalid_project_data)
+
+        # Then
+        self.assertFalse(project_serializer.is_valid())
+        self.assertEqual(
+            project_serializer.errors["title"][0], "This field is required."
+        )
+
+    def test_project_creation_with_missing_description(self):
+        # Given
+        invalid_project_data = self.create_valid_project_data()
+        del invalid_project_data["description"]
+
+        # When
+        project_serializer = ProjectSerializer(data=invalid_project_data)
+
+        # Then
+        self.assertFalse(project_serializer.is_valid())
+        self.assertEqual(
+            project_serializer.errors["description"][0], "This field is required."
+        )
+
+    def test_project_creation_with_non_string_title(self):
+        # Given
+        invalid_project_data = self.create_valid_project_data()
+        invalid_project_data["title"] = 123
+
+        # When
+        project_serializer = ProjectSerializer(data=invalid_project_data)
+
+        # Then
+        self.assertFalse(project_serializer.is_valid())
+        self.assertEqual(
+            project_serializer.errors["title"][0], "Must be a valid string."
+        )
+
+    def test_project_creation_with_non_string_description(self):
+        # Given
+        invalid_project_data = self.create_valid_project_data()
+        invalid_project_data["description"] = 123
+
+        # When
+        project_serializer = ProjectSerializer(data=invalid_project_data)
+
+        # Then
+        self.assertFalse(project_serializer.is_valid())
+        self.assertEqual(
+            project_serializer.errors["description"][0], "Must be a valid string."
+        )
